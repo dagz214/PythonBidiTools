94200de3aba27e735472082b116e5f6ebec66d71
dagz214 <58767064+dagz214@users.noreply.github.com>
2021-04-15 00:05:08 +0300

    fixed formatting

--- bidicli.py
+++ bidicli.py
@@ -1,9 +1,8 @@
 import click
 from python_bidi_tools import get_display_mod
+import os
+import sys
 
-# @click.command()
-# def cli():
-#     click.echo("Hello World")
 
 @click.command()
 @click.argument('string')
@@ -11,6 +10,7 @@ def main(string):
     # click.echo(click.style(string+'B', fg='magenta'))
     # click.echo(get_display_mod(string))
     click.echo(click.style(get_display_mod(string), fg='magenta'))
+
 
 if __name__ == "__main__":
     main()
